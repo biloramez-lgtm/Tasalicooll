56678bdac3c439d51f72ecf5f157ebe05c87d919
Mister Milian <biloramez@gmail.com>
2026-02-14 14:48:50 +0200

    Update GameEffect.kt

--- app/src/main/java/com/tasalicool/game/viewmodel/GameEffect.kt
+++ app/src/main/java/com/tasalicool/game/viewmodel/GameEffect.kt
@@ -1,6 +1,27 @@
 package com.tasalicool.game.viewmodel
 
 sealed class GameEffect {
-    data class ShowToast(val message: String) : GameEffect()
+
+    /* -------- Navigation -------- */
     object NavigateToGameOver : GameEffect()
+    object NavigateToLobby : GameEffect()
+
+    /* -------- UI Feedback -------- */
+    data class ShowSnackbar(val message: String) : GameEffect()
+    data class ShowDialog(
+        val title: String,
+        val message: String
+    ) : GameEffect()
+
+    /* -------- Animations -------- */
+    object PlayCardAnimation : GameEffect()
+    object PlayBidAnimation : GameEffect()
+
+    /* -------- Sound / Haptics -------- */
+    object PlayCardSound : GameEffect()
+    object VibrateOnTurn : GameEffect()
+
+    /* -------- Multiplayer -------- */
+    object SendGameStateToPeer : GameEffect()
+    object SyncWithPeer : GameEffect()
 }
