f18ae74716db8a743815c757ab26aaa49e721dc3
Mister Milian <biloramez@gmail.com>
2026-02-14 19:42:42 +0200

    Update AppNavGraph.kt

--- app/src/main/java/com/tasalicool/game/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/com/tasalicool/game/ui/navigation/AppNavGraph.kt
@@ -1,1 +1,42 @@
-.
+package com.tasalicool.game.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.tasalicool.game.ui.screens.*
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Game : Screen("game")
+    object Multiplayer : Screen("multiplayer")
+    object Settings : Screen("settings")
+}
+
+@Composable
+fun AppNavGraph(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(
+                onStartGame = {
+                    navController.navigate(Screen.Game.route)
+                },
+                onViewLeaderboard = {}
+            )
+        }
+        
+        composable(Screen.Game.route) {
+            // Game Screen
+        }
+        
+        composable(Screen.Multiplayer.route) {
+            MultiplayerScreen()
+        }
+    }
+}
