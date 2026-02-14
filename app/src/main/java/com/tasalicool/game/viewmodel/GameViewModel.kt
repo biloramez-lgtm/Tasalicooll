aac215c80e8070298f4cae2268d461a999f8cb37
Mister Milian <biloramez@gmail.com>
2026-02-14 13:34:02 +0200

    Update GameViewModel.kt

--- app/src/main/java/com/tasalicool/game/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/tasalicool/game/viewmodel/GameViewModel.kt
@@ -2,47 +2,48 @@ package com.tasalicool.game.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.tasalicool.game.engine.GameEngine
-import com.tasalicool.game.model.*
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.tasalicool.game.engine.ComprehensiveGameEngine
+import com.tasalicool.game.model.Card
+import com.tasalicool.game.model.Game
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class GameViewModel : ViewModel() {
-    private val gameEngine = GameEngine()
+class GameViewModel(
+    private val engine: ComprehensiveGameEngine
+) : ViewModel() {
 
-    private val _gameState = MutableStateFlow<Game?>(null)
-    val gameState: StateFlow<Game?> = _gameState
+    val gameState: StateFlow<Game?> = engine.gameState
+    val errorState = engine.error
 
-    fun initializeGame(player1Name: String, player2Name: String, ai1Name: String, ai2Name: String) {
+    fun startGame(
+        team1Name: String,
+        team2Name: String
+    ) {
         viewModelScope.launch {
-            val player1 = Player(id = 0, name = player1Name, isAI = false)
-            val player3 = Player(id = 2, name = player2Name, isAI = false)
-            val player2 = Player(id = 1, name = ai1Name, isAI = true)
-            val player4 = Player(id = 3, name = ai2Name, isAI = true)
-
-            val team1 = Team(id = 1, name = "Team 1", player1 = player1, player2 = player3)
-            val team2 = Team(id = 2, name = "Team 2", player1 = player2, player2 = player4)
-
-            val game = gameEngine.initializeGame(team1, team2, 0)
-            gameEngine.dealCards(game)
-            _gameState.emit(game)
+            engine.initializeDefaultGame(
+                team1Name = team1Name,
+                team2Name = team2Name
+            )
         }
     }
 
     fun placeBid(playerIndex: Int, bid: Int) {
         viewModelScope.launch {
-            val game = _gameState.value ?: return@launch
-            gameEngine.placeBid(game, playerIndex, bid)
-            _gameState.emit(game)
+            engine.gameState.value?.let {
+                engine.placeBid(it, playerIndex, bid)
+            }
         }
     }
 
     fun playCard(playerIndex: Int, card: Card) {
         viewModelScope.launch {
-            val game = _gameState.value ?: return@launch
-            gameEngine.playCard(game, playerIndex, card)
-            _gameState.emit(game)
+            engine.gameState.value?.let {
+                engine.playCard(it, playerIndex, card)
+            }
         }
+    }
+
+    fun clearError() {
+        engine.clearError()
     }
 }
