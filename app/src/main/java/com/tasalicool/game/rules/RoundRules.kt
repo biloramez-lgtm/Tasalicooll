595dc82a64a0bc1238503364efd5d08ac674626b
Mister Milian <biloramez@gmail.com>
2026-02-14 17:24:44 +0200

    Update RoundRules.kt

--- app/src/main/java/com/tasalicool/game/rules/RoundRules.kt
+++ app/src/main/java/com/tasalicool/game/rules/RoundRules.kt
@@ -1,1 +1,28 @@
-.
+package com.tasalicool.game.rules
+
+/**
+ * RoundRules - قوانين الجولات
+ * 
+ * الجولة الواحدة = 13 خدعة
+ */
+object RoundRules {
+    
+    /**
+     * عدد الخدعات في الجولة الواحدة
+     */
+    fun getTricksPerRound(): Int = 13
+    
+    /**
+     * هل الجولة اكتملت
+     */
+    fun isRoundComplete(tricksPlayed: Int): Boolean {
+        return tricksPlayed == getTricksPerRound()
+    }
+    
+    /**
+     * هل يمكن بدء جولة جديدة
+     */
+    fun canStartNewRound(isGameOver: Boolean): Boolean {
+        return !isGameOver
+    }
+}
