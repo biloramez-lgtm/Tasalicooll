466e02c9e46ea436e911836ea75ecfb00ce478c1
Mister Milian <biloramez@gmail.com>
2026-02-14 13:30:32 +0200

    Create GameMapper.kt

---
+++ app/src/main/java/com/tasalicool/game/mapper/GameMapper.kt
@@ -0,0 +1,19 @@
+package com.tasalicool.game.mapper
+
+import com.tasalicool.game.model.Game
+import com.tasalicool.game.data.GameEntity
+
+fun Game.toEntity(): GameEntity {
+    return GameEntity(
+        gameId = id,
+        team1Name = team1.name,
+        team2Name = team2.name,
+        team1Score = team1.score,
+        team2Score = team2.score,
+        winnerTeamId = winningTeamId ?: 0,
+        totalRounds = roundsPlayed,
+        gameMode = gameMode.name,
+        duration = duration,
+        playerCount = players.size
+    )
+}
