70e0754242b8405ba2fa641be3c5dced41f77282
Mister Milian <biloramez@gmail.com>
2026-02-14 14:56:26 +0200

    Update ConnectionStatus.kt

--- app/src/main/java/com/tasalicool/game/viewmodel/ConnectionStatus.kt
+++ app/src/main/java/com/tasalicool/game/viewmodel/ConnectionStatus.kt
@@ -1,21 +1,11 @@
 package com.tasalicool.game.viewmodel
 
 enum class ConnectionStatus {
-
-    IDLE,
-
-    /* Host */
-    CREATING_ROOM,
-    WAITING_FOR_PLAYERS,
-
-    /* Client */
-    JOINING_ROOM,
-
-    /* Active */
-    CONNECTED,
-    SYNCING,
-
-    /* Failure */
-    ERROR,
-    DISCONNECTED
+    IDLE,                // Multiplayer غير مفعّل
+    CREATING_ROOM,       // Host ينشئ غرفة
+    WAITING_FOR_PLAYERS, // Host ينتظر
+    CONNECTING,          // Client يحاول الاتصال
+    CONNECTED,           // تم الاتصال
+    SYNCING,             // مزامنة اللعبة
+    ERROR                // خطأ اتصال
 }
