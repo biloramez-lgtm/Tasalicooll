2185e06cc3d7cf879b9a2b7bbbdc1fcf5acd90e9
Mister Milian <biloramez@gmail.com>
2026-02-15 23:52:03 +0200

    Create color.kt

---
+++ app/src/main/res/values/color.kt
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <!-- الألوان الأساسية -->
+    <color name="primary">#1976D2</color>
+    <color name="onPrimary">#FFFFFF</color>
+    <color name="primaryContainer">#BBDEFB</color>
+    <color name="onPrimaryContainer">#000000</color>
+
+    <!-- ألوان ثانوية -->
+    <color name="secondary">#4CAF50</color>
+    <color name="onSecondary">#FFFFFF</color>
+    <color name="secondaryContainer">#C8E6C9</color>
+    <color name="onSecondaryContainer">#000000</color>
+
+    <!-- خلفيات -->
+    <color name="background">#121212</color>
+    <color name="surface">#1E1E1E</color>
+    <color name="error">#F44336</color>
+</resources>
